4b9a804e281780d6b8c1f44cf8bf5220eda4d97c
jmsparrowk <jmsparrowk@naver.com>
2024-08-06 23:22:50 +0900

    8/6

--- src/main/java/re1/demo/MemberRepositoryImpl.java
+++ src/main/java/re1/demo/MemberRepositoryImpl.java
@@ -3,11 +3,14 @@ package re1.demo;
 public class MemberRepositoryImpl implements MemberRepository{
     @Override
     public void save(Member member) {
-
     }
 
     @Override
     public Member findById(Long memberId) {
         return null;
+    }
+
+    @Override
+    public Member findByName(String name) {
     }
 }
