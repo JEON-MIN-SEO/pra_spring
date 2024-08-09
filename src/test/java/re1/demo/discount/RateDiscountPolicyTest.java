cf9efce233a0d6d5c8edfa14ebbd81d817d48835
jmsparrowk <jmsparrowk@naver.com>
2024-08-09 16:42:08 +0900

    8/9

--- src/test/java/re1/demo/discount/RateDiscountPolicyTest.java
+++ src/test/java/re1/demo/discount/RateDiscountPolicyTest.java
@@ -1,6 +1,7 @@
 package re1.demo.discount;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import re1.demo.Member;
 import re1.demo.enums.Grade;
@@ -12,6 +13,7 @@ class RateDiscountPolicyTest {
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
+    @DisplayName("vipだった場合")
     void vip() {
         //given
         Member member = new Member(1L, "VIP", Grade.VIP);
@@ -21,4 +23,14 @@ class RateDiscountPolicyTest {
         Assertions.assertThat(discount).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("vipじゃなかった場合")
+    void noVip() {
+        //given
+        Member member = new Member(1L, "BASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
 }
